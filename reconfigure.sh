16a65f970851ddc29769ccb4f1234ba108461f85
devensikotarasayalife425 <deven.sikotara@sayalife.in>
2025-01-27 14:37:03 +0530

    remove pipeline

--- reconfigure.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ~/.pub-cache/
-flutter pub cache repair
-# find . -type f -name '*.g.dart' -print -delete && find . -type f -name '*.freezed.dart' -print -delete && find . -type f -name '
-flutter clean
-rm pubspec.lock
-flutter pub get
-flutter packages pub run build_runner build --delete-conflicting-outputs
