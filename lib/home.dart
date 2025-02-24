7a4bb3839da8019961765270628dba57e7ca620d
devensikotarasayalife425 <deven.sikotara@sayalife.in>
2025-02-24 17:24:29 +0530

    Comment whole code

--- lib/home.dart
+++ lib/home.dart
@@ -1,57 +1,57 @@
-import 'package:external_path/external_path.dart';
-import 'package:flutter/material.dart';
+// import 'package:external_path/external_path.dart';
+// import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+// class Home extends StatefulWidget {
+//   const Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
 
-class _HomeState extends State<Home> {
+// class _HomeState extends State<Home> {
 
-  @override
-  void initState() {
-getPath();
+//   @override
+//   void initState() {
+// getPath();
 
-    getPublicDirectoryPath();
-    super.initState();
-  }
+//     getPublicDirectoryPath();
+//     super.initState();
+//   }
 
- List<String> exPath = [];
-   Future<void> getPath() async {
-    List<String> paths;
-    // getExternalStorageDirectories() will return list containing internal storage directory path
-    // And external storage (SD card) directory path (if exists)
-    paths = await ExternalPath.getExternalStorageDirectories();
+//  List<String> exPath = [];
+//    Future<void> getPath() async {
+//     List<String> paths;
+//     // getExternalStorageDirectories() will return list containing internal storage directory path
+//     // And external storage (SD card) directory path (if exists)
+//     paths = await ExternalPath.getExternalStorageDirectories();
 
-    setState(() {
-      exPath = paths; // [/storage/emulated/0, /storage/B3AE-4D28]
-    });
-  }
+//     setState(() {
+//       exPath = paths; // [/storage/emulated/0, /storage/B3AE-4D28]
+//     });
+//   }
 
-   Future<void> getPublicDirectoryPath() async {
-    String path;
+//    Future<void> getPublicDirectoryPath() async {
+//     String path;
 
-    path = await ExternalPath.getExternalStoragePublicDirectory(
-        ExternalPath.DIRECTORY_DCIM);
+//     path = await ExternalPath.getExternalStoragePublicDirectory(
+//         ExternalPath.DIRECTORY_DCIM);
 
-    setState(() {
-      print(" path: $path"); // /storage/emulated/0/Download
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:const Text("Deven 123"),
-      ),
-      body:ListView.builder(
-          itemCount: exPath.length,
-          itemBuilder: (context, index) {
-            print("Index:$index");
-            return Center(child: Text(exPath[index]));
-          }),
-    );
-  }
-}
+//     setState(() {
+//       print(" path: $path"); // /storage/emulated/0/Download
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title:const Text("Deven 123"),
+//       ),
+//       body:ListView.builder(
+//           itemCount: exPath.length,
+//           itemBuilder: (context, index) {
+//             print("Index:$index");
+//             return Center(child: Text(exPath[index]));
+//           }),
+//     );
+//   }
+// }
